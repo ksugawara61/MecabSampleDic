1044b5857d01a0f1c972d1a7b4715c272777249a
ksugawara61 <katsuya@kokohadoko.net>
2019-05-12 00:47:02 +0900

    [fix] コンテンツをイチローに変更

--- bin/make_model.py
+++ bin/make_model.py
@@ -2,7 +2,7 @@ from gensim.models import word2vec
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus('./sentence/sample-userdic-wakati.txt')
+sentences = word2vec.Text8Corpus('./sentence/ichiro-wakati.txt')
 
-model = word2vec.Word2Vec(sentences, size=200, min_count=1, window=15)
+model = word2vec.Word2Vec(sentences, size=200, min_count=5, window=15)
 model.save("./model/app.model")
